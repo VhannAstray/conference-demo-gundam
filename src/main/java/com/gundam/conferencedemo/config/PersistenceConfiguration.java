0f3cab9e913a0955db0bd5172bd1ab841b48cd67
D'OVIDIO <vincent.d-ovidio@capgemini.com>
2020-03-09 16:11:30 +0100

    Update for external TomCat mode.

--- src/main/java/com/gundam/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/gundam/conferencedemo/config/PersistenceConfiguration.java
@@ -9,11 +9,11 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-  /* Updated @Bean
+    @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
         System.out.println("My Custom datasource bean has been initialized and set");
         return builder.build();
-    }*/
+    }
 }
